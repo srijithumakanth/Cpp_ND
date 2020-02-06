9d987539cfd6243d48aee8ff52a5eabd792a37ff
srijith <sumakanthil@innovative-logistics.com>
2020-02-06 16:43:32 -0600

    Starting a thread with member functions

---
+++ Devel/concurrency/Starting a thread with member functions/example_2.cpp
@@ -0,0 +1,38 @@
+/* In example_1.cpp, we have to ensure that the existence of v2 
+outlives the completion of the thread t2 - otherwise there will be an 
+attempt to access an invalidated memory address. An alternative is to use 
+a heap-allocated object and a reference-counted pointer such as std::shared_ptr<Vehicle> 
+to ensure that the object lives as long as it takes the thread to finish its work. 
+The following example shows how this can be implemented: */
+
+#include <iostream>
+#include <thread>
+
+class Vehicle
+{
+public:
+    Vehicle() : _id(0) {}
+    void addID(int id) { _id = id; }
+    void printID()
+    {
+        std::cout << "Vehicle ID=" << _id << std::endl;
+    }
+
+private:
+    int _id;
+};
+
+int main()
+{
+    // create thread
+    std::shared_ptr<Vehicle> v(new Vehicle);
+    std::thread t = std::thread(&Vehicle::addID, v, 1); // call member function on object v
+    
+    // wait for thread to finish
+    t.join();
+    
+    // print Vehicle id
+    v->printID();
+    
+    return 0;
+}
