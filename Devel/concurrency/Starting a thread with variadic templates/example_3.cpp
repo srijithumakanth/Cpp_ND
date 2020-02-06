6492ca1f4d2b1eb61d43d97cb2b8fcbd6f9d4522
srijith <sumakanthil@innovative-logistics.com>
2020-02-06 16:10:45 -0600

    Starting threads with variadic templates

---
+++ Devel/concurrency/Starting a thread with variadic templates/example_3.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <thread>
+#include <string>
+
+void printName(std::string &name, int waitTime)
+{
+    std::this_thread::sleep_for(std::chrono::milliseconds(waitTime));
+    name += " (from Thread)";
+    std::cout << name << std::endl;
+}
+
+int main()
+{
+    std::string name("MyThread");
+    // std::string name = "MyThread";
+
+    // starting thread
+    std::thread t(printName, std::ref(name), 50);
+    // std::thread t(printName, name, 50); // std::ref() needs to be used when passing variables as a reference inside a variadic template.
+
+    // wait for thread before returning
+    t.join();
+
+    // print name from main
+    name += " (from Main)";
+    std::cout << name << std::endl;
+
+    return 0;
+}
