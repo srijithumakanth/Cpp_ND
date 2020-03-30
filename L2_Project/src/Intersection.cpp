08a77345b4a7634af2c6bb6b2e44500f6e242702
srijith <sumakanthil@innovative-logistics.com>
2020-03-30 02:58:05 -0500

    Task L2.2 completed

--- L2_Project/src/Intersection.cpp
+++ L2_Project/src/Intersection.cpp
@@ -63,6 +63,14 @@ void Intersection::addVehicleToQueue(std::shared_ptr<Vehicle> vehicle)
 
     // L2.2 : First, add the new vehicle to the waiting line by creating a promise, a corresponding future and then adding both to _waitingVehicles. 
     // Then, wait until the vehicle has been granted entry. 
+    
+    std::promise<void> prmsVehiclesAllowedToEnter;
+    std::future<void> ftrVehiclesAllowedToEnter = prmsVehiclesAllowedToEnter.get_future();
+
+    _waitingVehicles.pushBack(vehicle, std::move(prmsVehiclesAllowedToEnter));
+
+    // Wait until the vehicle has been granted entry
+    ftrVehiclesAllowedToEnter.wait();
 
     std::cout << "Intersection #" << _id << ": Vehicle #" << vehicle->getID() << " is granted entry." << std::endl;
 }
